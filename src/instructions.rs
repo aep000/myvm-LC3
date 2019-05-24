5bcb8c588d21be0ed58fb7a3fff16f4858f46c48
aep000 <alexparson1@gmail.com>
2019-05-24 10:57:25 -0400

    Basic version finished

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,10 +1,13 @@
 use crate::memory::Context;
 use crate::memory::mem_read;
 use crate::memory::Registers;
+use std::str;
+use std::io::{self, Read};
+use std::process;
 
 fn sign_extend(x: u16, bit_count:usize) -> u16
 {
-    let extended = 0;
+    let extended = x;
     if (((x >> (bit_count - 1)) & 1)==0) {
         let extended = x | (0xFFFF << bit_count);
     }
@@ -24,6 +27,14 @@ fn update_flags(r:usize, context: &mut Context)
     else
     {
         context.Reg[RNum!(Registers::RCOND)] = Conds::FL_POS as u16;
+    }
+}
+
+pub fn getc() -> char {
+    let mut buf: [u8; 1] = [0; 1];
+    return match io::stdin().read(&mut buf) {
+        Ok(_) => buf[0] as char,
+        Err(_) => 255 as char
     }
 }
 
@@ -47,23 +58,32 @@ enum TrapCode
 impl TrapCode{
     pub fn run(&self, context: &mut Context, instr: &usize){
         match self{
-            GETC => {
-
+            TrapCode::GETC => {
+                context.Reg[RNum!(Registers::R0)] = getc() as u16;
             },  /* get character from keyboard, not echoed onto the terminal */
-            OUT => {
+            TrapCode::OUT => {
+                print!("{}", context.Reg[RNum!(Registers::R0)] as u8 as char);
 
             },   /* output a character */
-            PUTS => {
-
+            TrapCode::PUTS => {
+                let mut c = context.Reg[RNum!(Registers::R0)];
+                while(context.Memory[c as usize]!=0){
+                    print!("{}", str::from_utf8(&[context.Memory[c as usize] as u8]).expect("not UTF-8"));
+                    c+=1;
+                }
             },  /* output a word string */
-            IN => {
-
+            TrapCode::IN => {
+                println!("Enter Character");
+                let c = getc();
+                println!("{}", c);
+                context.Reg[RNum!(Registers::R0)] = c as u16;
             },    /* get character from keyboard, echoed onto the terminal */
-            PUTSP => {
+            TrapCode::PUTSP => {
 
             }, /* output a byte string */
-            HALT => {
-                
+            TrapCode::HALT => {
+                println!("\nProgram Halted");
+                process::exit(1);
             }
         }
     }
@@ -93,15 +113,19 @@ pub enum Instruction{
 impl Instruction{
     pub fn Run(&self, context: &mut Context, instr: &u16){
         let instr = *instr as usize;
-        match self{
-            BR => {
+        match *self{
+            Instruction::BR => {
+                #[cfg(debug_assertions)]
+                println!("BR");
                 let conds = ((instr >> 9) & 0x7) as u16;
                 let offset = sign_extend((instr & 0x1ff) as u16, 9);
                 if(conds & context.Reg[RNum!(Registers::RCOND)] == conds){
                     context.Reg[RNum!(Registers::RPC)] = context.Reg[RNum!(Registers::RPC)] + offset;
                 }
             }, /* branch */
-            ADD => {
+            Instruction::ADD => {
+                #[cfg(debug_assertions)]
+                println!("ADD");
 
                 let dest = (instr >> 9) & 0x7;
                 let r1 = (instr >> 6) & 0x7; //first source reg
@@ -119,19 +143,24 @@ impl Instruction{
                 }
                 update_flags(dest, context);
             },    /* add  */
-            LD => {
-
+            Instruction::LD => {
+                #[cfg(debug_assertions)]
+                println!("LD");
                 let dest = (instr >> 9) & 0x7;
                 let offset = sign_extend((instr & 0x1ff) as u16, 9); //Location in memory at Offset + PC which we are loading from
                 context.Reg[dest] = mem_read((context.Reg[RNum!(Registers::RPC)]+offset) as usize,context);
                 update_flags(dest, context);
             },     /* load */
-            ST => {
+            Instruction::ST => {
+                #[cfg(debug_assertions)]
+                println!("ST");
                 let src = (instr >> 9) & 0x7;
                 let offset = sign_extend((instr & 0x1ff) as u16, 9);
                 context.Memory[(context.Reg[RNum!(Registers::RPC)]+offset) as usize]= context.Reg[src];
             },     /* store */
-            JSR => {
+            Instruction::JSR => {
+                #[cfg(debug_assertions)]
+                println!("JSR");
                 let flag = (instr >> 11) & 0x1;
                 context.Reg[RNum!(Registers::R7)] = context.Reg[RNum!(Registers::RPC)];
                 if(flag == 0){
@@ -142,8 +171,9 @@ impl Instruction{
                     context.Reg[RNum!(Registers::RPC)] = context.Reg[RNum!(Registers::RPC)] + sign_extend((instr & 0x3ff) as u16, 11);
                 }
             },    /* jump register */
-            AND => {
-
+            Instruction::AND => {
+                #[cfg(debug_assertions)]
+                println!("AND");
                 let dest = (instr >> 9) & 0x7;
                 let r1 = (instr >> 6) & 0x7; //first source reg
                 let immediatMode = (instr >> 5) & 0x1;
@@ -160,58 +190,94 @@ impl Instruction{
                 }
                 update_flags(dest, context);
             },    /* bitwise and */
-            LDR => {
-
+            Instruction::LDR => {
+                #[cfg(debug_assertions)]
+                println!("LDR");
                 let dest = (instr >> 9) & 0x7;
                 let baseR = (instr >> 6) & 0x7;
                 let offset:u16 = sign_extend((instr & 0x3F) as u16, 6); //Location in memory at Offset + PC which we are loading from
                 context.Reg[dest] = mem_read(((context.Reg[baseR] as u16)+offset) as usize,context);
                 update_flags(dest, context);
             },    /* load register */
-            STR => {
+            Instruction::STR => {
+                #[cfg(debug_assertions)]
+                println!("STR");
                 let src = (instr >> 9) & 0x7;
                 let dest = (instr >> 6) & 0x7;
                 let offset = sign_extend((instr & 0x3F) as u16, 6);
                 context.Memory[(context.Reg[dest]+offset) as usize]= context.Reg[src];
             },    /* store register */
-            RTI => {},    /* unused */
-            NOT => {
+            Instruction::RTI => {},    /* unused */
+            Instruction::NOT => {
+                #[cfg(debug_assertions)]
+                println!("NOT");
                 let dest = (instr >> 9) & 0x7;
                 let src = (instr >> 6) & 0x7;
                 context.Reg[dest] = !context.Reg[src];
                 update_flags(dest, context);
             },    /* bitwise not */
-            LDI => {
-
+            Instruction::LDI => {
+                #[cfg(debug_assertions)]
+                println!("LDI");
                 let dest = (instr >> 9) & 0x7;
                 let offset = sign_extend((instr & 0x1ff) as u16, 9); //Location in memory at Offset + PC contains the location we are loading from
                 context.Reg[dest] = mem_read(mem_read(context.Reg[RNum!(Registers::RPC)] as usize,context) as usize,context);
                 update_flags(dest, context);
             },    /* load indirect */
-            STI => {
+            Instruction::STI => {
+                #[cfg(debug_assertions)]
+                println!("STI");
                 let src = (instr >> 9) & 0x7;
                 let offset = sign_extend((instr & 0x1ff) as u16, 9);
                 context.Memory[context.Memory[(context.Reg[RNum!(Registers::RPC)]+offset) as usize] as usize]= context.Reg[src];
             },    /* store indirect */
-            JMP => {
+            Instruction::JMP => {
+                #[cfg(debug_assertions)]
+                println!("JMP");
                 let src = (instr >> 6) & 0x7;
                 context.Reg[RNum!(Registers::RPC)] = context.Reg[src];
             },    /* jump */
-            RES => {},    /* reserved (unused) */
-            LEA => {
+            Instruction::RES => {},    /* reserved (unused) */
+            Instruction::LEA => {
+                #[cfg(debug_assertions)]
+                println!("LEA");
                 let dest = (instr >> 9) & 0x7;
                 let offset = sign_extend((instr & 0x1ff) as u16, 9);
                 context.Reg[dest] = context.Reg[RNum!(Registers::RPC)] + offset;
                 update_flags(dest, context);
             },    /* load effective address */
-            TRAP => {
+            Instruction::TRAP => {
+                #[cfg(debug_assertions)]
+                println!("TRAP");
                 let code = instr & 0xFF;
                 if code >= 0x20 && code <= 0x25{
                     let code: TrapCode = unsafe { std::mem::transmute(code as i8) };
                     code.run(context, &instr);
                 }
             },    /* execute trap */
-            ICOUNT => (),
+            Instruction::ICOUNT => (),
+        }
+    }
+
+    pub fn fromOpcode(opcode:u16)->Instruction{
+        match opcode{
+            0 => Instruction::BR,
+            1 => Instruction::ADD,
+            2 => Instruction::LD,
+            3 => Instruction::ST,
+            4 => Instruction::JSR,
+            5 => Instruction::AND,
+            6 => Instruction::LDR,
+            7 => Instruction::STR,
+            8 => Instruction::RTI,
+            9 => Instruction::NOT,
+            10 => Instruction::LDI,
+            11 => Instruction::STI,
+            12 => Instruction::JMP,
+            13 => Instruction::RES,
+            14 => Instruction::LEA,
+            15 => Instruction::TRAP,
+            _ => Instruction::ICOUNT
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 mod memory;
 mod instructions;
 
+use std::env;
+use std::io::Read;
+use std::fs::File;
+use std::path::Path;
+
 use memory::Registers;
 use instructions::Instruction;
 use memory::Context;
@@ -9,16 +14,51 @@ use memory::Context;
 const PC_START:u16  = 0x3000;
 
 
+fn get16(mem: &[u8], ind: usize) -> u16 {
+    ((mem[ind] as u16) << 8) + mem[ind+1] as u16
+}
+
+// https://doc.rust-lang.org/rust-by-example/std_misc/file/open.html
+fn read_image(mem: &mut [u16], image_path: &str) -> u32 {
+    let path = Path::new(image_path);
+     // println!("[*] Loading {}", path.to_str().unwrap());
+    let mut file = File::open(&path).expect("Couldn't open file.");
+
+    const SIZE: u32 = std::u16::MAX as u32 * 2 - 2;
+    let mut mem_buffer: [u8; SIZE as usize] = [0; SIZE as usize];
+    file.read(&mut mem_buffer).expect("Couldn't read file.");
+    let length = file.metadata().unwrap().len();
+     // println!("[*] File length {}", length);
+
+    let base = get16(&mem_buffer, 0) as usize;
+    for i in (2..length).step_by(2) {
+        // println!("{}",i);
+        mem[base+(i/2 - 1) as usize] = get16(&mem_buffer, i as usize);
+    }
+    // println!("{:?}", &mem[0x3000..0x4000]);
+    length as u32
+}
+
 fn main() {
     let mut context: Context = Context::new();
-    let mut running = 1;
+    let args: Vec<String> = env::args().collect();
+    for image in args.iter().skip(1) {
+        read_image(&mut context.Memory, image);
+    }
     context.Reg[RNum!(Registers::RPC)] = PC_START;
+    let mut running = true;
+    while running{
+        let instr: u16 = context.Memory[context.Reg[Registers::RPC as usize] as usize];
 
-    let instr: u16 = context.Reg[Registers::RPC as usize];
-    context.Reg[RNum!(Registers::RPC)]+=1;
-    let op = instr >> 12;
-    if(op< Instruction::ICOUNT as u16){
-        let op: Instruction = unsafe { std::mem::transmute(op as i8) };
-        op.Run(&mut context,&instr);
+        context.Reg[RNum!(Registers::RPC)]+=1;
+        let op = instr >> 12;
+        if op == 0{
+            break;
+        }
+        //println!("{}",op);
+        if(op< Instruction::ICOUNT as u16){
+            let op: Instruction = Instruction::fromOpcode(op);
+            op.Run(&mut context,&instr);
+        }
     }
 }
